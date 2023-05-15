e9362204c31e2bdbf4dc0ec47dfc5ccb5cd78876
swornimgrg <swornimgrg94@gmail.com>
2023-05-15 23:45:22 +0100

    Store data in sessionStorage

--- client/src/context/MoviesContext.jsx
+++ client/src/context/MoviesContext.jsx
@@ -4,21 +4,27 @@ import MovieFinder from "../apis/MovieFinder";
 export const MoviesContext = createContext();
 
 export const MoviesContextProvider = (props) => {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState(sessionStorage.getItem("data") || null);
+
+  const storeData = (value) => {
+    const serializedData = JSON.stringify(value);
+    sessionStorage.setItem("data", serializedData);
+    setMovies(value);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await MovieFinder.get("/");
-        setMovies(response.data.movies);
-        console.log(movies);
+        const dataToStore = response.data.movies;
+        storeData(dataToStore);
       } catch (err) {
         console.log(err);
       }
     };
 
     fetchData();
-  }, []);
+  }, [setMovies]);
 
   return (
     <MoviesContext.Provider value={{ movies }}>
